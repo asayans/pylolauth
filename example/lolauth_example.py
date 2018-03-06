258aacd2eed9140da4a5321be21a4036840e387b
Alex (sysfiend) Sayáns <pitagoras4444@hotmail.es>
2018-03-06 12:58:42 +0100

    Create lolauth_example.py

---
+++ example/lolauth_example.py
@@ -0,0 +1,28 @@
+from pynput.keyboard import Key, Controller
+import time
+import subprocess
+
+# Password
+pass = "1 Lik3 Che3Se"
+
+# For example: \Riot Games\League of Legends\LeagueClient.exe
+subprocess.call(['\Riot Games\League of Legends\LeagueClient.exe'])
+
+# Wait untill client is fully launched so 
+# we can star typing the password field.
+# This value can (should) be modified to fit
+# your personal timing.
+time.sleep(5.5)  
+
+keyboard = Controller()
+
+# Type password
+for a in pass:
+	keyboard.press(a)
+	keyboard.release(a)
+
+time.sleep(0.5)
+
+# Press enter
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
